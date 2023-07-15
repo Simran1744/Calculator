2a0729bfeea14e2cc9b7a427ff740ae23690ff0f
simran1744 <simran1744@gmail.com>
2023-07-15 14:32:09 +0200

    Day 2

--- src/CalculatorBox.java
+++ src/CalculatorBox.java
@@ -1,13 +1,19 @@
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.lang.invoke.CallSite;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.Locale;
 
-    public class CalculatorBox extends JFrame{
-        JButton btnAdd, btnSubtract, btnDivide, btnMultiply, btnClear, btnDelete, btnEquals, btnDot;
+
+public class CalculatorBox extends JFrame{
+        JButton btnAdd, btnSubtract, btnDivide, btnMultiply, btnClear, btnDelete, btnEquals, btnDot, btnMod, btnNeg;
         JButton[] numBtn;
         JTextField output;
-        String previous, current, operator;
+        String previous = "";
+        String current = "";
+        String operator;
 
 
     public CalculatorBox(){
@@ -21,6 +27,7 @@ import java.awt.*;
         JPanel row5 = new JPanel();
 
         output = new JTextField(16);
+        output.setEditable(false);
         btnSubtract = new JButton("-");
         btnAdd = new JButton("+");
         btnDivide = new JButton("÷");
@@ -29,6 +36,13 @@ import java.awt.*;
         btnEquals = new JButton("=");
         btnClear = new JButton("C");
         btnDelete = new JButton("D");
+        btnMod = new JButton("%");
+        btnNeg = new JButton("¬");
+
+
+        NumberBtnHandler numBtnHandler = new NumberBtnHandler();
+        OtherBtnHandler otherBtnHandler = new OtherBtnHandler();
+        OperatorBtnHandler opBtnHandler = new OperatorBtnHandler();
 
         numBtn = new JButton[11];
         numBtn[10] = btnDot;
@@ -36,7 +50,11 @@ import java.awt.*;
         for(int count = 0; count < numBtn.length -1; count++){
             numBtn[count] = new JButton(String.valueOf(count));
             numBtn[count].setFont(new Font("Monospaced", Font.BOLD, 22));
+            numBtn[count].addActionListener(numBtnHandler);
         }
+
+
+
 
         btnDot.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnEquals.setFont(new Font("Monospaced", Font.BOLD, 22));
@@ -44,14 +62,33 @@ import java.awt.*;
         btnSubtract.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnDivide.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnMultiply.setFont(new Font("Monospaced", Font.BOLD, 22));
-        btnClear.setFont(new Font("Monospaced", Font.BOLD, 20));
-        btnDelete.setFont(new Font("Monospaced", Font.BOLD, 20));
+        btnClear.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnDelete.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnMod.setFont(new Font("Monospaced", Font.BOLD, 22));
+        btnNeg.setFont(new Font("Monospaced", Font.BOLD, 22));
 
         output.setMaximumSize(new Dimension(185,40));
         output.setFont(new Font("Monospaced", Font.BOLD, 27));
         output.setDisabledTextColor(new Color(0,0,0));
         output.setMargin(new Insets(0,5,0,0));
-            output.setText("0");
+        output.setText("0");
+
+        // Add action listeners to other buttons
+        btnDot.addActionListener(numBtnHandler);
+        btnDelete.addActionListener(otherBtnHandler);
+        btnClear.addActionListener(otherBtnHandler);
+        btnEquals.addActionListener(otherBtnHandler);
+
+        // Add action listeners to operation buttons
+        btnMultiply.addActionListener(opBtnHandler);
+        btnAdd.addActionListener(opBtnHandler);
+        btnSubtract.addActionListener(opBtnHandler);
+        btnDivide.addActionListener(opBtnHandler);
+        btnMod.addActionListener(opBtnHandler);
+        btnNeg.addActionListener(opBtnHandler);
+
+        output.setText("0");
+
 
         row1.setLayout(new BoxLayout(row1, BoxLayout.LINE_AXIS));
         row2.setLayout(new BoxLayout(row2, BoxLayout.LINE_AXIS));
@@ -59,7 +96,9 @@ import java.awt.*;
         row4.setLayout(new BoxLayout(row4, BoxLayout.LINE_AXIS));
         row5.setLayout(new BoxLayout(row5, BoxLayout.LINE_AXIS));
 
-        row1.add(Box.createHorizontalGlue());
+        //row1.add(Box.createHorizontalGlue());
+        row1.add(btnNeg);
+        row1.add(btnMod);
         row1.add(btnClear);
         row1.add(btnDelete);
         row2.add(numBtn[7]);
@@ -96,27 +135,151 @@ import java.awt.*;
     }
 
         public void delete() {
+            if(current.length() > 0) {
+                current = current.substring(0, current.length() - 1);
+            }
         }
 
         public void clear() {
+            previous = "";
+            current = "";
+            operator = null;
         }
 
         public void updateOutput(){
+            output.setText(current);
+            if(current.length() == 0){
+                output.setText("0");
+            }
         }
 
         public void appendToOutput(String num) {
+            if(num.equals(".") && current.contains(".")){
+                return;
+            }
+            current += num;
         }
 
         public void selectOperator(String newOperator) {
+            if(current.isEmpty()){
+                operator = newOperator;
+                return;
+            }
+
+            if(!previous.isEmpty()){
+                calculate();
+            }
+
+            operator = newOperator;
+            previous = current;
+            current = "";
         }
 
         public void calculate() {
+
+            if(previous.length() < 1 || current.length() < 1){
+                return;
+            }
+
+            double result = 0.0;
+            double num1 = Double.parseDouble(previous);
+            double num2 = Double.parseDouble(current);
+
+            switch (operator){
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "÷":
+                    if(num2 != 0) result = num1 / num2;
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+                default:
+                    break;
+            }
+            current = String.valueOf(result);
+            operator = null;
+            previous = "";
+            prettyString();
         }
 
-        //Hier weitermachen
+        public void prettyString(){
+            if(current.length() > 0){
+                String integer = current.split("\\.")[0];
+                String decimal = current.split("\\.")[1];
+                if(decimal.equals("0")){
+                    current = integer;
+                }
+            }
+
+        }
+
+        private class NumberBtnHandler implements ActionListener{
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JButton selectedBtn = (JButton) e.getSource();
+                for(JButton btn : numBtn){
+                    if (selectedBtn == btn) {
+                        appendToOutput(btn.getText());
+                        updateOutput();
+                    }
+                }
+            }
+        }
+
+        private class OperatorBtnHandler implements  ActionListener{
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton selectedBtn = (JButton) e.getSource();
+                if(selectedBtn == btnMultiply){
+                    selectOperator(btnMultiply.getText());
+                }
+                else if(selectedBtn == btnAdd){
+                    selectOperator(btnAdd.getText());
+                }
+                else if(selectedBtn == btnSubtract){
+                    selectOperator(btnSubtract.getText());
+                }
+                else if(selectedBtn == btnDivide){
+                    selectOperator(btnDivide.getText());
+                }
+                else if(selectedBtn == btnMod){
+                    selectOperator(btnMod.getText());
+                }
+                else if(selectedBtn == btnNeg){
+                    current = String.valueOf(Integer.parseInt(current) * (-1));
+                }
+                updateOutput();
+            }
+        }
+
+        private class OtherBtnHandler implements ActionListener{
+            @Override
+            public void actionPerformed(ActionEvent e){
+                JButton selectedBtn = (JButton) e.getSource();
+                if(selectedBtn == btnDelete){
+                    delete();
+                }
+                else if(selectedBtn == btnClear){
+                    clear();
+                }
+                else if (selectedBtn == btnEquals){
+                    calculate();
+                }
+                updateOutput();
+            }
+        }
 
     public static void main(String[] args){
         new CalculatorBox();
     }
+
 
     }
